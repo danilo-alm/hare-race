ab7d3def8233b024ebf6d7cb2ebd39699809874d
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-08-21 07:43:16 -0300

    organize function declarations
    move functions around to improve readability

--- race.py
+++ race.py
@@ -11,22 +11,47 @@ from values import (
 )
 
 
-def print_race_state(hares: list[Hare]):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    state = ''
-    for hare in hares:
-        dist = int(hare.track_distance)
-        dist = RACE_DISTANCE if dist > RACE_DISTANCE else dist
-        dist_left = RACE_DISTANCE - dist
-        state += f'{hare.id_normalized} - {"#" * dist}{" " * dist_left}|\n'
-    print(state)
+def main():
+    countdown()
+
+    ranking = []
+    ranking_lock = threading.Lock()
+    semaphore = Semaphore(1)
+    race_updated = threading.Condition()
+
+    hare_names = {
+        0: 'Danilo',
+        1: 'Marcos Paulo',
+        2: 'Everton',
+        3: 'Tércio',
+        4: 'Programador Web'
+    }
+
+    hares, threads = create_hare_threads(hare_names, semaphore, ranking, ranking_lock, race_updated)
+    
+    monitor_thread = threading.Thread(target=monitor_race, args=(hares, race_updated))
+    monitor_thread.start()
+
+    start_and_join_threads(threads)
+    monitor_thread.join()
+
+    display_ranking(ranking)
 
 
-def monitor_race(hares: list[Hare], race_updated: threading.Condition):
-    while any(hare.track_distance < RACE_DISTANCE for hare in hares):
-        with race_updated:
-            race_updated.wait(MAX_REST_SECONDS)
-        print_race_state(hares)
+def countdown():
+    print(r'''
+ _   _    _    ____  _____   ____      _    ____ _____ 
+| | | |  / \  |  _ \| ____| |  _ \    / \  / ___| ____|
+| |_| | / _ \ | |_) |  _|   | |_) |  / _ \| |   |  _|  
+|  _  |/ ___ \|  _ <| |___  |  _ <  / ___ \ |___| |___ 
+|_| |_/_/   \_\_| \_\_____| |_| \_\/_/   \_\____|_____|
+    ''')
+
+    for i in range(3, 0, -1):
+        print(i, end='... ')
+        sys.stdout.flush()
+        time.sleep(1)
+    print('GO!')
 
 
 def hare_behaviour(hare: Hare, semaphore: Semaphore, ranking: list, ranking_lock: threading.Lock, race_updated: threading.Condition):
@@ -47,22 +72,6 @@ def hare_behaviour(hare: Hare, semaphore: Semaphore, ranking: list, ranking_lock
             semaphore.release()
 
         hare.rest()
-
-
-def countdown():
-    print(r'''
- _   _    _    ____  _____   ____      _    ____ _____ 
-| | | |  / \  |  _ \| ____| |  _ \    / \  / ___| ____|
-| |_| | / _ \ | |_) |  _|   | |_) |  / _ \| |   |  _|  
-|  _  |/ ___ \|  _ <| |___  |  _ <  / ___ \ |___| |___ 
-|_| |_/_/   \_\_| \_\_____| |_| \_\/_/   \_\____|_____|
-    ''')
-
-    for i in range(3, 0, -1):
-        print(i, end='... ')
-        sys.stdout.flush()
-        time.sleep(1)
-    print('GO!')
 
 
 def create_hare_threads(hare_names, semaphore, ranking, ranking_lock, race_updated):
@@ -90,31 +99,22 @@ def display_ranking(ranking):
         print(f'{position}{suffix} - {hare.id} ({hare.jumps} jumps)')
 
 
-def main():
-    countdown()
+def print_race_state(hares: list[Hare]):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    state = ''
+    for hare in hares:
+        dist = int(hare.track_distance)
+        dist = RACE_DISTANCE if dist > RACE_DISTANCE else dist
+        dist_left = RACE_DISTANCE - dist
+        state += f'{hare.id_normalized} - {"#" * dist}{" " * dist_left}|\n'
+    print(state)
 
-    ranking = []
-    ranking_lock = threading.Lock()
-    semaphore = Semaphore(1)
-    race_updated = threading.Condition()
 
-    hare_names = {
-        0: 'Danilo',
-        1: 'Marcos Paulo',
-        2: 'Everton',
-        3: 'Tércio',
-        4: 'Programador Web'
-    }
-
-    hares, threads = create_hare_threads(hare_names, semaphore, ranking, ranking_lock, race_updated)
-    
-    monitor_thread = threading.Thread(target=monitor_race, args=(hares, race_updated))
-    monitor_thread.start()
-
-    start_and_join_threads(threads)
-    monitor_thread.join()
-
-    display_ranking(ranking)
+def monitor_race(hares: list[Hare], race_updated: threading.Condition):
+    while any(hare.track_distance < RACE_DISTANCE for hare in hares):
+        with race_updated:
+            race_updated.wait(MAX_REST_SECONDS)
+        print_race_state(hares)
 
 
 if __name__ == '__main__':
