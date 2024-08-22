0bc02f0bba71014b190783c271ad3421f2e09dde
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-08-22 17:41:35 -0300

    fix semaphore implementation

--- semaphore.py
+++ semaphore.py
@@ -2,15 +2,20 @@ import threading
 
 
 class Semaphore:
-    def __init__(self, initial):
-        self.value = initial
-        self.lock = threading.Lock()
+    def __init__(self, value=1):
+        if value < 0:
+            raise ValueError("Semaphore initial value must be >= 0")
+        self._value = value
+        self._value_lock = threading.Lock()
+        self._nonzero = threading.Condition(self._value_lock)
 
     def acquire(self):
-        with self.lock:
-            if self.value > 0:
-                self.value -= 1
+        with self._nonzero:
+            while self._value == 0:
+                self._nonzero.wait()
+            self._value -= 1
 
     def release(self):
-        with self.lock:
-            self.value += 1
+        with self._nonzero:
+            self._value += 1
+            self._nonzero.notify()
